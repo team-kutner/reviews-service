a405321bb4c454dcc80ea528e0439da8aecbaa5c
Mukhtar Bahadory <mukhtar5.bahadory5@gmail.com>
2020-11-10 19:43:27 -0600

    created chanceRating script

--- db/utils/seed.js
+++ db/utils/seed.js
@@ -1,5 +1,13 @@
 require('../mongoose');
+const faker = require('faker');
+
 
 const Review = require('../review');
 const Comment = require('../comment');
+
+for (let i = 1; i <= 100; i++) {
+  const firstName = faker.firstName();
+
+
+}
 
