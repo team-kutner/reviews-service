b255cdd3e6ba6d213fce805f3fcadd16ef8b45cb
Mukhtar Bahadory <mukhtar5.bahadory5@gmail.com>
2020-11-21 16:52:47 -0600

    concat

--- client/index.js
+++ client/index.js
@@ -3,5 +3,6 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import Header from './header/Header'
 ReactDOM.render(<App />, document.getElementById('ReviewsApp'));
+
 ReactDOM.render(<Header />, document.getElementById('Header'));
 
