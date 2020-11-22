68b82b903ade4700674139cd89f8e694c24ea440
Mukhtar Bahadory <mukhtar5.bahadory5@gmail.com>
2020-11-21 18:03:45 -0600

    full support for global modules

--- client/index.js
+++ client/index.js
@@ -1,5 +1,7 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+// import React from 'react';
+// import ReactDOM from 'react-dom';
+const {React} = window
+const {ReactDOM} = window
 import App from './App';
 import Header from './header/Header'
 ReactDOM.render(<App />, document.getElementById('ReviewsApp'));
